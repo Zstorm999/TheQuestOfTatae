89b4cc049d6a76bd6e17254c8a6d4fec3e307d36
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2021-03-03 18:38:06 +0100

    Trying to get this commit to work

--- Unity/TheQuestOfTatae/Assets/Scripts/Entities/Characters/PlayerController.cs
+++ Unity/TheQuestOfTatae/Assets/Scripts/Entities/Characters/PlayerController.cs
@@ -13,10 +13,15 @@ public class PlayerController : MonoBehaviour
     private float velocity;
 
     [SerializeField]
-    [Range(0f, .5f)]
-    private float deadZone;
+    [Range(0f, 1f)]
+    private float startDeadZone;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float endDeadZone;
 
     private float xMov, yMov;
+    private float xMovPrev, yMovPrev;
 
     [SerializeField]
     private Direction dirPrimary;
@@ -28,7 +33,7 @@ public class PlayerController : MonoBehaviour
     private Action action;
 
     private Direction newDirPrimary;
-   
+
 
     private void Start()
     {
@@ -44,8 +49,15 @@ public class PlayerController : MonoBehaviour
         xMov = Input.GetAxis("Horizontal");
         yMov = Input.GetAxis("Vertical");
 
-        xMov = applyDeadZone(xMov);
-        yMov = applyDeadZone(yMov);
+        float xMovTmp = applyDeadZone(xMov, xMovPrev);
+        float yMovTmp = applyDeadZone(yMov, yMovPrev);
+
+        xMovPrev = xMov;
+        yMovPrev = yMov;
+
+        xMov = xMovTmp;
+        yMov = yMovTmp;
+
 
 
         playSpeed = Mathf.Sqrt(xMov * xMov + yMov * yMov);
@@ -61,20 +73,18 @@ public class PlayerController : MonoBehaviour
 
         if (yMov > 0)
         {
-            //animator.changeAnimState(Direction.UP, Action.WALK);
             action = Action.WALK;
             setNewDir(Direction.UP);
         }
         else if (yMov < 0)
         {
-            //animator.changeAnimState(Direction.DOWN, Action.WALK);
             action = Action.WALK;
             setNewDir(Direction.DOWN);
 
         }
 
         //IDLE
-        if(xMov == 0 && yMov == 0)
+        if (xMov == 0 && yMov == 0)
         {
             action = Action.NONE;
             playSpeed = 1f;
@@ -95,25 +105,39 @@ public class PlayerController : MonoBehaviour
         //xMov = Mathf.Round(xMov);
         //yMov = Mathf.Round(yMov);
 
-        
 
-        Debug.Log("x: " + xMov + ",y: " + yMov);
-        Vector3 newPos = new Vector3(xMov, yMov, 0)*Time.deltaTime*velocity;
+
+        //Debug.Log("x: " + xMov + ",y: " + yMov);
+        Vector3 newPos = new Vector3(xMov, yMov, 0) * Time.deltaTime * velocity;
 
         transform.position += newPos;
 
     }
 
-    private float applyDeadZone(float var)
+    private float applyDeadZone(float var, float varPrev)
     {
         if (var == 0) return 0;
 
         int sign = (var < 0) ? -1 : 1;
-        var = Mathf.Abs(var);
 
-        if (var < deadZone) var = 0f;
 
-        return var * sign;
+        //here we have to check if we are accelerating or decelerating.
+
+        float varAbs = Mathf.Abs(var);
+        float varPrevAbs = Mathf.Abs(varPrev);
+
+
+        if (varAbs > varPrevAbs)
+        {
+
+            if (varAbs < startDeadZone) return 0;
+        }
+        else if(varAbs < varPrevAbs)
+        {
+            if (varAbs < endDeadZone) return 0;
+        }
+
+        return var;
     }
 
     private void setNewDir(Direction dir)
