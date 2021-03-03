b781190c0c2537bb9db7cd71f5c842e3f35690ee
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2021-03-03 18:33:20 +0100

    Modified deadzones for player walk input

--- Unity/TheQuestOfTatae/Assets/Scripts/Entities/Characters/PlayerController.cs
+++ Unity/TheQuestOfTatae/Assets/Scripts/Entities/Characters/PlayerController.cs
@@ -13,10 +13,15 @@ public class PlayerController : MonoBehaviour
     private float velocity;
 
     [SerializeField]
-    [Range(0f, .5f)]
-    private float deadZone;
+    [Range(0f, 1f)]
+    private float startDeadZone;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float endDeadZone;
 
     private float xMov, yMov;
+    private float xMovPrev, yMovPrev;
 
     [SerializeField]
     private Direction dirPrimary;
@@ -44,8 +49,15 @@ public class PlayerController : MonoBehaviour
         xMov = Input.GetAxis("Horizontal");
         yMov = Input.GetAxis("Vertical");
 
-        xMov = applyDeadZone(xMov);
-        yMov = applyDeadZone(yMov);
+        float xMovTmp = applyDeadZone(xMov, xMovPrev);
+        float yMovTmp = applyDeadZone(yMov, yMovPrev);
+
+        xMovPrev = xMov;
+        yMovPrev = yMov;
+
+        xMov = xMovTmp;
+        yMov = yMovTmp;
+
 
 
         playSpeed = Mathf.Sqrt(xMov * xMov + yMov * yMov);
@@ -61,13 +73,11 @@ public class PlayerController : MonoBehaviour
 
         if (yMov > 0)
         {
-            //animator.changeAnimState(Direction.UP, Action.WALK);
             action = Action.WALK;
             setNewDir(Direction.UP);
         }
         else if (yMov < 0)
         {
-            //animator.changeAnimState(Direction.DOWN, Action.WALK);
             action = Action.WALK;
             setNewDir(Direction.DOWN);
 
@@ -97,23 +107,46 @@ public class PlayerController : MonoBehaviour
 
         
 
-        Debug.Log("x: " + xMov + ",y: " + yMov);
+        //Debug.Log("x: " + xMov + ",y: " + yMov);
         Vector3 newPos = new Vector3(xMov, yMov, 0)*Time.deltaTime*velocity;
 
         transform.position += newPos;
 
     }
 
-    private float applyDeadZone(float var)
+    private float applyDeadZone(float var, float varPrev)
     {
         if (var == 0) return 0;
 
         int sign = (var < 0) ? -1 : 1;
+<<<<<<< Updated upstream
         var = Mathf.Abs(var);
 
         if (var < deadZone) var = 0f;
 
         return var * sign;
+=======
+
+        //here we have to check if we are accelerating or decelerating.
+
+        float varAbs = Mathf.Abs(var);
+        float varPrevAbs = Mathf.Abs(varPrev);
+
+
+        if (varAbs > varPrevAbs)
+        {
+
+            if (varAbs < startDeadZone) return 0;
+        }
+        else if(varAbs < varPrevAbs)
+        {
+            if (varAbs < endDeadZone) return 0;
+        }
+
+        return var;
+
+        
+>>>>>>> Stashed changes
     }
 
     private void setNewDir(Direction dir)
